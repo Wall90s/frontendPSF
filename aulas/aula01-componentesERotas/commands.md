65bb30aea55afe27a0bfaac1cff9bb5c43acb28e
wall90s <wallacesb@outlook.com.br>
2023-06-25 12:05:29 -0300

    Organizando pastas

---
+++ aulas/aula01-componentesERotas/commands.md
@@ -0,0 +1,10 @@
+# Comandos utilizados
+
+- ng new <i>aula01</i>
+<br> Cria um nobo projeto Angular
+
+- ng generate component <i>views/novo-component</i>
+<br> Gera um componente novo
+
+[paleta de cores]
+(https://coolors.co/ffe5ec-ffc2d1-ff8fab-fb6f92-883955)
---
+++ aulas/aula01-componentesERotas/src/app/views/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { SobreComponent } from './sobre/sobre.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'sobre', component: SobreComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
